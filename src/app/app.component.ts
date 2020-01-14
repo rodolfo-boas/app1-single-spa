603aa4944a46f8632188eedec352af5c4fcdc24b
rodolfo.boas <rodolfo.boas@7comm.com.br>
2020-01-14 18:36:44 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'rod-root',
+  selector: 'micro-rod-app1',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
