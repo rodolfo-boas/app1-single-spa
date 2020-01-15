e19ff114a4f2b79b9f1dd972ff583922b13b8f93
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-01-14 23:41:12 -0300

    criado observable e função de leitura e gravação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { restoreView } from '@angular/core/src/render3';
 
 @Component({
   selector: 'micro-rod-app1',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
+
 export class AppComponent {
   title = 'app1';
+
+  valorForm: BehaviorSubject<any> = new BehaviorSubject(null);
+  valorForm$: Observable<any> = this.valorForm.asObservable();
+
   onSubmit(f: NgForm) {
+    console.log(f)
     console.log(f.value);  // { first: '', last: '' }
     console.log(f.valid);  // false
+    this.valorForm.next(f.value)
   }
+
+  lerObservable() {
+    this.valorForm$.subscribe(
+      retorno => {
+        console.log(retorno)
+      }
+    )
+  }
+
 }
